45c3058b32a8c9f66b28d277f9e296c837da8b9d
Thalisson-Alves <thalisson2030@gmail.com>
2020-11-01 22:46:07 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# Flappy-Bird
+# Flappy Bird
+
+# Table of content
+- [About](#About)
+- [How to Run](#How-to-run)
+- [How to build](#How-to-build)
+- [Features](#Features)
  
+# About
+A clone of the Flappy Bird game made with the [SFML](https://www.sfml-dev.org/) library. Game recreated for the purpose of learning more about neural networks and game development.
+
+# How to run
+There is an executable for windows in the "Game.zip" folder
+
+# How to build
+  ## Requirements
+  - [CMake](https://cmake.org/)
+  - [SFML](https://www.sfml-dev.org/)
+  - [c++ compiler](https://sourceforge.net/projects/mingw-w64/)  
+  * Follow this [tutorial](https://www.sfml-dev.org/tutorials/2.5/compile-with-cmake.php) for more detail
+
+  ## Cloning
+   ```bash
+  # Clone repository
+  $ git clone https://github.com/Thalisson-Alves/Flappy-Bird
+  
+  # Go to Flappy Bird directory
+  $ cd Flappy-Bird
+  ```
+
+  ## Building
+  1. Switch to the build directory
+  1. Type the make command of the compiler you are using. For example type "mingw32-make" if you use MinGW.
+  1. Run the executable :)
+
+# Features
+Some features that I'm working on
+- [ ] End game state
+- [ ] Score
+- [ ] Neural network
+    - [ ] Load a model
+    - [ ] Save a model
+- [ ] Music / sound
+- [ ] Settings state
---
+++ build/CMakeFiles/game.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/thali/OneDrive/Documentos/Personal_projects/cpp_games/flappy_bird/src/Application.cpp" "C:/Users/thali/OneDrive/Documentos/Personal_projects/cpp_games/flappy_bird/build/CMakeFiles/game.dir/src/Application.cpp.obj"
+  "C:/Users/thali/OneDrive/Documentos/Personal_projects/cpp_games/flappy_bird/src/Bird.cpp" "C:/Users/thali/OneDrive/Documentos/Personal_projects/cpp_games/flappy_bird/build/CMakeFiles/game.dir/src/Bird.cpp.obj"
+  "C:/Users/thali/OneDrive/Documentos/Personal_projects/cpp_games/flappy_bird/src/Button.cpp" "C:/Users/thali/OneDrive/Documentos/Personal_projects/cpp_games/flappy_bird/build/CMakeFiles/game.dir/src/Button.cpp.obj"
+  "C:/Users/thali/OneDrive/Documentos/Personal_projects/cpp_games/flappy_bird/src/Component.cpp" "C:/Users/thali/OneDrive/Documentos/Personal_projects/cpp_games/flappy_bird/build/CMakeFiles/game.dir/src/Component.cpp.obj"
+  "C:/Users/thali/OneDrive/Documentos/Personal_projects/cpp_games/flappy_bird/src/Container.cpp" "C:/Users/thali/OneDrive/Documentos/Personal_projects/cpp_games/flappy_bird/build/CMakeFiles/game.dir/src/Container.cpp.obj"
+  "C:/Users/thali/OneDrive/Documentos/Personal_projects/cpp_games/flappy_bird/src/Entity.cpp" "C:/Users/thali/OneDrive/Documentos/Personal_projects/cpp_games/flappy_bird/build/CMakeFiles/game.dir/src/Entity.cpp.obj"
+  "C:/Users/thali/OneDrive/Documentos/Personal_projects/cpp_games/flappy_bird/src/GameState.cpp" "C:/Users/thali/OneDrive/Documentos/Personal_projects/cpp_games/flappy_bird/build/CMakeFiles/game.dir/src/GameState.cpp.obj"
+  "C:/Users/thali/OneDrive/Documentos/Personal_projects/cpp_games/flappy_bird/src/MenuState.cpp" "C:/Users/thali/OneDrive/Documentos/Personal_projects/cpp_games/flappy_bird/build/CMakeFiles/game.dir/src/MenuState.cpp.obj"
+  "C:/Users/thali/OneDrive/Documentos/Personal_projects/cpp_games/flappy_bird/src/PauseState.cpp" "C:/Users/thali/OneDrive/Documentos/Personal_projects/cpp_games/flappy_bird/build/CMakeFiles/game.dir/src/PauseState.cpp.obj"
+  "C:/Users/thali/OneDrive/Documentos/Personal_projects/cpp_games/flappy_bird/src/Pipe.cpp" "C:/Users/thali/OneDrive/Documentos/Personal_projects/cpp_games/flappy_bird/build/CMakeFiles/game.dir/src/Pipe.cpp.obj"
+  "C:/Users/thali/OneDrive/Documentos/Personal_projects/cpp_games/flappy_bird/src/Random.cpp" "C:/Users/thali/OneDrive/Documentos/Personal_projects/cpp_games/flappy_bird/build/CMakeFiles/game.dir/src/Random.cpp.obj"
+  "C:/Users/thali/OneDrive/Documentos/Personal_projects/cpp_games/flappy_bird/src/State.cpp" "C:/Users/thali/OneDrive/Documentos/Personal_projects/cpp_games/flappy_bird/build/CMakeFiles/game.dir/src/State.cpp.obj"
+  "C:/Users/thali/OneDrive/Documentos/Personal_projects/cpp_games/flappy_bird/src/StateStack.cpp" "C:/Users/thali/OneDrive/Documentos/Personal_projects/cpp_games/flappy_bird/build/CMakeFiles/game.dir/src/StateStack.cpp.obj"
+  "C:/Users/thali/OneDrive/Documentos/Personal_projects/cpp_games/flappy_bird/src/Utility.cpp" "C:/Users/thali/OneDrive/Documentos/Personal_projects/cpp_games/flappy_bird/build/CMakeFiles/game.dir/src/Utility.cpp.obj"
+  "C:/Users/thali/OneDrive/Documentos/Personal_projects/cpp_games/flappy_bird/src/World.cpp" "C:/Users/thali/OneDrive/Documentos/Personal_projects/cpp_games/flappy_bird/build/CMakeFiles/game.dir/src/World.cpp.obj"
+  "C:/Users/thali/OneDrive/Documentos/Personal_projects/cpp_games/flappy_bird/src/main.cpp" "C:/Users/thali/OneDrive/Documentos/Personal_projects/cpp_games/flappy_bird/build/CMakeFiles/game.dir/src/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/Program Files (x86)/SFML/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
